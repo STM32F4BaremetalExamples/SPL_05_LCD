d33a05aa0ae6fdb925db0188f7b968f8fccca093
Manuel Limones <limonesu.me@gmail.com>
2015-07-10 02:36:13 -0500

    Refactor LCD controller *Extract hardware dependent operations *Add new files to nbproject *Add new files to keil project

---
+++ source/LCD_stm32f4.c
@@ -0,0 +1,26 @@
+#include "LCD_hw.h"
+#include "stm32f4xx.h"                  // Device header;
+
+void lcd_hw_init(void){
+	
+}
+
+void lcd_hw_outputCtrl(unsigned char ctrl_pin, unsigned char newState){
+	
+}
+
+void lcd_hw_output_LowNibble(unsigned char lowNibbleData){
+	
+}
+
+void lcd_hw_output_data(unsigned char data){
+	
+}
+
+void lcd_hw_delay_us(int delay_time){
+
+}
+
+void lcd_hw_delay_ms(int delay_time){
+	
+}
